956521ddfe5ed5cba5d058b6ce3de9e4f946f7b1
Nefedovich <126053619+Nefedovich@users.noreply.github.com>
2024-09-26 14:25:34 +0300

    Add files via upload

---
+++ Parser1.cpp
@@ -0,0 +1,21 @@
+#include "Parcer.h"
+
+
+vector<Point> Parcer::parce() {
+    double x, y;
+    std::ifstream F(filename);
+    vector<Point> path(0);
+    if (F.is_open() == 0) {
+        cout << "cannot read file!" << endl;
+        return path;
+    }
+    int n;
+    F >> n;
+    // char s[100];
+    for (int i = 0; i < n; i++) {
+        F >> x >> y;
+        path.push_back(Point(x, y));
+    }
+    F.close();
+    return path;
+}
